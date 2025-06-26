c2b5bc455c15fc3cf4e401dc29c15bb904a15eb8
ahmedfathy5012 <44382585+ahmedfathy5012@users.noreply.github.com>
2025-06-26 14:32:58 +0300

    fonts

--- resources/views/layouts/adminindex.blade.php
+++ resources/views/layouts/adminindex.blade.php
@@ -27,8 +27,11 @@ License: You must have a valid license purchased only from themeforest(the above
 	<link rel="canonical" href="https://keenthemes.com/metronic" />
 	<!--begin::Fonts-->
 	<meta name="csrf-token" content="{{ csrf_token() }}">
-<link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@800&display=swap" rel="stylesheet">
+<!-- <link rel="preconnect" href="https://fonts.gstatic.com"> -->
+<!-- <link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@800&display=swap" rel="stylesheet"> -->
+ <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@200;300;400;500;700;800;900&display=swap" rel="stylesheet">
 	<!--end::Fonts-->
 	
 	
