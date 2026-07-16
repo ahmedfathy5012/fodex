cba2b599b2d542f9bc19a3b8fc0a11070ccb9833
30o0mda <techlab.ahme.emad@gmail.com>
2026-07-16 15:13:51 +0300

    edite style

--- app/Http/Controllers/GenderController.php
+++ app/Http/Controllers/GenderController.php
@@ -1,77 +1,75 @@
-<?php 
+<?php
 
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Gender;
 use App\DataTables\GenderDataTable;
-class GenderController extends Controller 
+
+class GenderController extends Controller
 {
-
-  /**
-   * Display a listing of the resource.
-   *
-   * @return Response
-   */
-   public function index(GenderDataTable $dataTable)
+    private function genderView(string $page): string
     {
-      // dd(Country::all());
-        return $dataTable->render('admindashboard.gender.index');
+        return env('APP_ENV') == 'production'
+            ? "admindashboard.gender.$page"
+            : "admindashboard.gender.V2.$page";
     }
-  /**
-   * Show the form for creating a new resource.
-   *
-   * @return Response
-   */
-  public function create()
-  {
-    return view('admindashboard.gender.create');
-  }
 
- 
-  public function store(Request $request)
-  {
-    $request->validate([
-      'title' => 'required'],[
-      'title.required' => 'هذا الحقل مطلوب'
-       ]);
-    $gender = new Gender;
-    $gender->title = $request->title;
-    $gender->save();
-    return redirect()->route('gender.index');
-  }
+    public function index(GenderDataTable $dataTable)
+    {
+        return $dataTable->render($this->genderView('index'));
+    }
 
- 
-  public function edit($id)
-  {
-    $gender = Gender::where('id',$id)->first();
-    return view('admindashboard.gender.edit')->with('gender',$gender); 
-  }
-  public function update(Request $request,$id)
-  {
-      $request->validate([
-      'title' => 'required'],[
-      'title.required' => 'هذا الحقل مطلوب'
-       ]);
-    $gender = Gender::where('id',$id)->first();
-    $gender->title = $request->title;
-    $gender->save();
-    return redirect()->route('gender.index');
-  }
+    public function create()
+    {
+        return view($this->genderView('create'));
+    }
 
-  /**
-   * Remove the specified resource from storage.
-   *
-   * @param  int  $id
-   * @return Response
-   */
-  public function destroy($id)
-  {
-     $gender = Gender::where('id',$id)->first();
-     $gender->delete();
-     return response()->json(['status' => true]);
-  }
-  
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+        ], [
+            'title.required' => 'هذا الحقل مطلوب',
+        ]);
+
+        $gender = new Gender;
+        $gender->title = $request->title;
+        $gender->save();
+
+        return redirect()->route('gender.index');
+    }
+
+    public function edit($id)
+    {
+        $gender = Gender::where('id', $id)->first();
+
+        return view($this->genderView('edit'))
+            ->with('gender', $gender);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'title' => 'required',
+        ], [
+            'title.required' => 'هذا الحقل مطلوب',
+        ]);
+
+        $gender = Gender::where('id', $id)->first();
+        $gender->title = $request->title;
+        $gender->save();
+
+        return redirect()->route('gender.index');
+    }
+
+    public function destroy($id)
+    {
+        $gender = Gender::where('id', $id)->first();
+        $gender->delete();
+
+        return response()->json([
+            'status' => true,
+        ]);
+    }
 }
-
-?>
